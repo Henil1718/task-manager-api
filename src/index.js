1169b00a741ed7cc0f607f7749d5028167c83af7
Henil <hnkakadiya2705@gmail.com>
2021-04-01 08:19:28 +0530

    Final commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(userRouter)
 app.use(taskRouter)
 
 app.get('', async (req, res) => {
-    res.send('<h1>Welcom to task-manager-api</h1>')
+    res.send('<h1>Welcome to task-manager-api</h1>')
 })
 
 app.listen(port, () => {
